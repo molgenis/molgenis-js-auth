72d98889dbd5732687d54bd858eb12be5385f596
Fleur Kelpin <f.kelpin@umcg.nl>
2021-01-21 10:59:13 +0100

    feat: authorisation using graphql-shield

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app" class="container">
-    <application-title class="mb-4" />
-    <message-container />
-    <b-tabs content-class="mt-3">
-      <b-tab active>
-        <template v-slot:title> <b-icon-people-fill /> Users </template>
-        <users-tab />
-      </b-tab>
-      <b-tab title="Roles">
-        <template v-slot:title> <b-icon-journals /> Roles </template>
-        <roles-tab />
-      </b-tab>
-    </b-tabs>
-  </div>
+    <div v-if="me && me.roles.includes('SU')">
+      {{ me }}
+      <application-title class="mb-4" />
+      <message-container />
+      <b-tabs content-class="mt-3">
+        <b-tab active>
+          <template v-slot:title> <b-icon-people-fill /> Users </template>
+          <users-tab />
+        </b-tab>
+        <b-tab title="Roles">
+          <template v-slot:title> <b-icon-journals /> Roles </template>
+          <roles-tab />
+        </b-tab>
+      </b-tabs>
+    </div>
+    <div v-else-if="me">
+      Authenticated, not SU
+      {{me}}
+    </div>
+    <div v-else>
+      Not authenticated
+    </div>
 </template>
 
 <script>
@@ -20,6 +29,7 @@ import UsersTab from './components/UsersTab'
 import ApplicationTitle from './components/ApplicationTitle.vue'
 import RolesTab from './components/RolesTab.vue'
 import MessageContainer from './components/MessageContainer.vue'
+import gql from 'graphql-tag'
 
 export default {
   name: 'app',
@@ -28,6 +38,14 @@ export default {
     UsersTab,
     RolesTab,
     MessageContainer
+  },
+  apollo: {
+    me: gql`{
+      me {
+        email
+        roles
+      }
+    }`
   }
 }
 </script>
--- packages/server/apollo/index.js
+++ packages/server/apollo/index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server-express')
 const resolvers = require("./resolvers")
+const permissions = require('./permissions')
 const FusionAPI = require("./FusionAPI")
+const { applyMiddleware } = require('graphql-middleware')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
 const fs = require("fs")
 const path = require("path")
 const typeDefs = fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8")
@@ -12,9 +15,10 @@ const dataSources = () => ({
 const context = ({ req }) => ({
   token: process.env.FUSION_API_TOKEN,
   applicationId: process.env.CLIENT_ID,
-  ...req.openid.user
+  user: req.openid.user
 })
 
-const apollo = new ApolloServer({ typeDefs, resolvers, dataSources, context })
+const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }), permissions)
+const apollo = new ApolloServer({ schema, dataSources, context })
 
 module.exports = apollo
